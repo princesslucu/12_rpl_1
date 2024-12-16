a5dafbaf5811115a5573f042897bbf5d5a99c63d
princesslucu <salsabilakeisha041@gmail.com>
2024-12-16 08:44:42 +0700

    commit pertama

--- models/M_contoh.php
+++ models/M_contoh.php
@@ -1,5 +1,15 @@
 <?php
 
 class M_contoh {
+    // bikin fungsi tampil, edit dan hapus
+    public function tampil(){
+        // fungsi untuk menampilkan data dari database
+    }
+    function edit(){
+        // fungsi untuk edit data dari database
+    }
+    function hapus(){
+        // fungsi untuk edit dari database
+    }
 }
 ?>
